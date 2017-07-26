a28174cc49fad8ff1a9200b72748eb4df1512af6
Narotam Singh <narotam.singh@gmail.com>
2017-07-26 21:32:24 +0530

    structure

--- wlogs_analysis.py
+++ wlogs_analysis.py
@@ -7,10 +7,27 @@ using the psycopg2 module to connect to the database.
 '''
 
 
+def connect(db_name=“news”):
+    """Connect to database. Returns a database connection """
+    try:
+        db = psycopg2.connect("dbname={}".format(db_name))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print "Unable to connect to the database",db_name
+
+def fetch_query(query):
+    """
+    Connect to the database, query, fetch results, close connection, return results
+    """
+    db, cursor = connect()
+    cursor.execute(query)
+    return cursor.fetchall()
+    db.close()
+
+        
 def article():
-    """Run query and prints most popular three articles of all time"""
-    db = psycopg2.connect(database="news")
-    c = db.cursor()
+    """Fetch and prints most popular three articles of all time"""
     Query = """
         SELECT articles.title, count(log.path) AS num
         FROM articles, log
@@ -19,17 +36,14 @@ def article():
         ORDER BY num DESC
         LIMIT 3
         """
-    c.execute(Query)
+    result = fetch_query(Query)
     print "\nMost popular three articles of all time:"
-    for item in c.fetchall():
+    for item in result:
         print '\"{}\" - {} views'.format(item[0], item[1])
-    db.close()
 
 
 def author():
-    """Run query and prints most popular article authors of all time"""
-    db = psycopg2.connect(database="news")
-    c = db.cursor()
+    """Fetch and prints most popular article authors of all time"""
     Query = """
         SELECT authors.name, count(log.path) AS num
         FROM authors, articles, log
@@ -38,18 +52,14 @@ def author():
         GROUP BY authors.name
         ORDER BY num DESC
     """
-    c.execute(Query)
+    result = fetch_query(Query)
     print "\nMost popular article authors of all time:"
-    for item in c.fetchall():
+    for item in result:
         print '{} - {} views'.format(item[0], item[1])
-    db.close()
 
 
 def error():
-    """Query and print days on which more than 1% of requests lead to errors"""
-    db = psycopg2.connect(database="news")
-    """Connect to the PostgreSQL database."""
-    c = db.cursor()
+    """Fetch and print days on which more than 1% of requests lead to errors"""
     Query = """
         SELECT DATE(time),
         round(SUM(CASE
@@ -62,12 +72,12 @@ def error():
                     END) * 100.00 / count(status) > 1.00
         ORDER BY One DESC
         """
-    c.execute(Query)
+    result = fetch_query(Query)
     print "\nOn which days did more than 1% of requests lead to errors:"
-    for item in c.fetchall():
+    for item in result:
         """Formating date here, save database time"""
         print '{:%B %d, %Y} - {}% errors\n'.format(item[0], item[1])
-    db.close()
+
 
 if __name__ == '__main__':
     article()
